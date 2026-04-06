84a1c089b975101f7e96d6e49bc21739885b3359
TheOutdoorProgrammer <joey@theoutdoorprogrammer.com>
2026-04-06 14:34:09 -0400

    feat: use python for package creation

--- autoscaler/function_package/package.sh
+++ autoscaler/function_package/package.sh
@@ -54,8 +54,20 @@ cp "$SCRIPT_DIR/host.json" "$PACKAGE_DIR/host.json"
 cp "$SCRIPT_DIR/AutoscalerTimer/function.json" "$PACKAGE_DIR/AutoscalerTimer/function.json"
 
 echo "Creating deployment package..."
-cd "${PACKAGE_DIR}"
-zip -r -q "$OUTPUT_ZIP" ./*
+# Use python to zip the directory since zip cli isnt on spacelift public workers
+python3 -c "
+import zipfile, os, sys
+
+source_dir = sys.argv[1]
+output_zip = sys.argv[2]
+
+with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as z:
+    for root, dirs, files in os.walk(source_dir):
+        for f in files:
+            full_path = os.path.join(root, f)
+            arc_name = os.path.relpath(full_path, source_dir)
+            z.write(full_path, arc_name)
+" "$PACKAGE_DIR" "$OUTPUT_ZIP"
 
 echo "Package created successfully at $OUTPUT_ZIP"
 echo "Package size: $(du -h "$OUTPUT_ZIP" | cut -f1)"
