c6304af034bb58333f08285319a617d15434fec9
jubran nassar <jubrann@spacelift.io>
2026-09-21 12:21:28 -0500

    Use SCSI-compatible VMSS SKU

--- variables.tf
+++ variables.tf
@@ -125,7 +125,7 @@ variable "os_disk_storage_account_type" {
 variable "vmss_sku" {
   type        = string
   description = "The VM SKU to use for the VMSS instances."
-  default     = "Standard_D2ads_v6"
+  default     = "Standard_D2ads_v5"
 }
 
 variable "worker_pool_id" {
